68a468a4fd039a238a71555655c5350d50d3c8fa
Josh Rogen <rogenjh@gmail.com>
2020-08-18 22:04:37 -0400

    created proxy server for zagat clone

---
+++ proxy-server/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const axios = require('axios')
+
+//grab the incoming id
+  //pass that id in a request to each of my components
+  //components to render based on the results of the request
+
+app.use('/:id', express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
