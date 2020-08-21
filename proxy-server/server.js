3fb749e4a75a2e76a644e1bdfeec5f1711a301a9
Josh Rogen <rogenjh@gmail.com>
2020-08-21 19:00:46 -0400

    added cors fr

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 4000
 const axios = require('axios')
+const cors = require('cors');
+
+app.use(cors());
 
 //grab the incoming id
   //pass that id in a request to each of my components
